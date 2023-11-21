92b13d8812ded265fe5d1bb7266bd0d9dbce974d
Teck-Meng <122603398+Teck-Meng@users.noreply.github.com>
2023-11-21 12:43:52 +0800

    CAMs v1.0

--- filehandler/test.java
+++
@@ -1,9 +0,0 @@
-package filehandler;
-
-public class test {
-    public static void main(String[] args) {
-        String s = "31";
-        int a = Integer.parseInt(s);
-        System.out.println(a);
-    }
-}
